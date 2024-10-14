8c654a6b86e79296f3f46b58d8c9b08837da87fb
Mikołaj Chmist <91905851+miki314@users.noreply.github.com>
2024-10-14 13:44:54 +0200

    task 4 and access key
    Finished task 4 and started to using access keys as an environmental variables

--- main.py
+++ main.py
@@ -21,13 +21,29 @@ def list_files_by_regex(client: any, bucket: str, prefix: str = "", regex_patter
         print(matching_patterns)
 
 def delete_files_by_regex(client: any, bucket: str, prefix: str = "", regex_pattern: str = "") -> None:
-    pass
+    response = client.list_objects_v2(Bucket = bucket, Prefix = prefix)
+    if 'Contents' in response:
+        matching_patterns = [obj['Key'] for obj in response['Contents'] if search(regex_pattern, obj['Key'])]
+
+        if matching_patterns:
+            objects_to_delete = [{'Key':file} for file in matching_patterns]
+
+            delete_response = client.delete_objects(
+                Bucket = bucket,
+                Delete = {'Objects': objects_to_delete}
+            )
+
+            deleted = delete_response.get('Deleted', [])
+            if deleted:
+                print(f"Deleted {len(deleted)} files:")
+                for obj in deleted:
+                    print(obj['Key'])
+        else: print("No files matched given pattern")
+    else: print("the bucket is empty or does not exists")
 
 def main() -> None:
     s3 = boto3.client(
         "s3",
-        aws_access_key_id = "access_key_id",
-        aws_secret_access_key = "access_key"
     )
 
     # Task 1
@@ -41,22 +57,30 @@ def main() -> None:
     local_file_path: str = './test_file.txt'
     s3_destination_key: str = 'b-wing/test_file.txt'
 
-    upload_file(
-        client= s3,
-        local_path= local_file_path,
-        destination_path= s3_destination_key,
-        bucket= bucket
-    )
+    # upload_file(
+    #     client= s3,
+    #     local_path= local_file_path,
+    #     destination_path= s3_destination_key,
+    #     bucket= bucket
+    # )
 
     # Task 3
 
     regex_pattern: str = r"[a-z]+_[a-z]+\.txt"
-    list_files_by_regex(
-        client=s3,
-        bucket=bucket,
-        prefix=folder,
-        regex_pattern=regex_pattern
-    )
+    # list_files_by_regex(
+    #     client=s3,
+    #     bucket=bucket,
+    #     prefix=folder,
+    #     regex_pattern=regex_pattern
+    # )
+
+    # Task 4
+    # delete_files_by_regex(
+    #     client=s3,
+    #     bucket=bucket,
+    #     prefix=folder,
+    #     regex_pattern=regex_pattern
+    # )
 
 if __name__ == '__main__':
     main()
